de723b043bea4c280e8e98ba3a6d1fa7cd6a3d80
ahmadreza <ahmadreza.shamimi@gmail.com>
2022-07-31 01:06:29 -0400

    Upload tech blog

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,9 +2,25 @@ import 'package:blog_tech/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:blog_tech/colors.dart';
+import 'package:blog_tech/home.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 4)).then((value) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
